1f4d16e350a62bd353e0428abf346f766bb3d1a7
melfarss <medlfarssi10@gmail.com>
2024-07-22 18:31:35 +0100

    my 9th commit

--- myfamily.sh
+++ myfamily.sh
@@ -1,16 +1,1 @@
-URL="https://learn.zone01oujda.ma/assets/superhero/all.json"
-
-if [ -z "$HERO_ID" ]; then
-  echo "HERO_ID is not set. Please export HERO_ID with the appropriate value."
-  exit 1
-fi
-
-relatives=$(curl -s "$URL" | jq -r --arg id "$HERO_ID" '
-  .[] | select(.id == ($id | tonumber)) | .connections.relatives
-')
-
-if [ -z "$relatives" ]; then
-  echo "No relatives found for HERO_ID $HERO_ID"
-else
-  echo "$relatives"
-fi
+curl https://learn.zone01oujda.ma/assets/superhero/all.json | jq '.[] | select(.id=='$HERO_ID') .connections | .relatives' | tr -d '"'
