8534c3100b0f94dda7e117b68926b840b710c874
lfarssi <2002060600366@ofppt-edu.ma>
2024-07-23 14:46:51 +0200

    my first go commit

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,8 +1,9 @@
 package main
 import "github.com/01-edu/z01"
 func main() {
-	for i := 97; i <= 122; i++ {
-		z01.PrintRune(rune(i))
+	var r rune
+	for r = 'a'; i <= 'z'; i++ {
+		z01.PrintRune(r)
 	}
 	z01.PrintRune('\n')
 }
