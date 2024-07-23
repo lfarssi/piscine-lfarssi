84f58598b946c00ba2bc9887a6fe2a396ee23567
lfarssi <2002060600366@ofppt-edu.ma>
2024-07-23 14:07:11 +0200

    my first commit in go lang

--- mastertheLS.sh
+++
@@ -1,6 +0,0 @@
-find . -maxdepth 1 -not -name '.*' -not -name '.' -not -name '..' -printf '%A@ %f\n' |
-sort -n |
-cut -d' ' -f2- |
-awk '{if (system("[ -d ""$0"" ]") == 0) print $0"/"; else print $0}' |
-tr '\n' ',' |
-sed 's/,$/\n/'
