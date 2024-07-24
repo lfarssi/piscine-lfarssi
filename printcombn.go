406192d4e2acdfc48590086f80aa1b4a67bda20e
melfarss <medlfarssi10@gmail.com>
2024-07-24 10:36:29 +0100

    audit

--- printcombn.go
+++ printcombn.go
@@ -1,18 +1,38 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import (
+    "fmt"
+    "github.com/01-edu/z01"
+)
 
-func IsdPositive(nb int) {
-	if nb < 0 {
-		z01.PrintRune('T')
-	} else {
-		z01.PrintRune('F')
-	}
-	z01.PrintRune('\n')
+func PrintCombN(n int) {
+    if n <= 0 || n >= 10 {
+        return
+    }
+
+    combinations := generateCombinations(n, 0, "")
+
+    for i, comb := range combinations {
+        for _, ch := range comb {
+            z01.PrintRune(ch)
+        }
+        if i < len(combinations)-1 {
+            z01.PrintRune(',')
+            z01.PrintRune(' ')
+        }
+    }
+    z01.PrintRune('\n')
 }
 
-func main2() {
-	IsdPositive(-10)
-	IsdPositive(-2)
-	IsdPositive(2)
+func generateCombinations(n int, start int, prefix string) []string {
+    if n == 0 {
+        return []string{prefix}
+    }
+
+    combinations := []string{}
+    for i := start; i <= 9; i++ {
+        newPrefix := fmt.Sprintf("%s%d", prefix, i)
+        combinations = append(combinations, generateCombinations(n-1, i+1, newPrefix)...)
+    }
+    return combinations
 }
