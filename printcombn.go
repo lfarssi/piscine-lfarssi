7d7912079924058697ee650358cc437af7ab0c82
melfarss <medlfarssi10@gmail.com>
2024-07-24 10:37:09 +0100

    audit

--- printcombn.go
+++ printcombn.go
@@ -1,38 +1,38 @@
 package piscine
 
 import (
-    "fmt"
-    "github.com/01-edu/z01"
+	"fmt"
+	"github.com/01-edu/z01"
 )
 
 func PrintCombN(n int) {
-    if n <= 0 || n >= 10 {
-        return
-    }
+	if n <= 0 || n >= 10 {
+		return
+	}
 
-    combinations := generateCombinations(n, 0, "")
+	combinations := generateCombinations(n, 0, "")
 
-    for i, comb := range combinations {
-        for _, ch := range comb {
-            z01.PrintRune(ch)
-        }
-        if i < len(combinations)-1 {
-            z01.PrintRune(',')
-            z01.PrintRune(' ')
-        }
-    }
-    z01.PrintRune('\n')
+	for i, comb := range combinations {
+		for _, ch := range comb {
+			z01.PrintRune(ch)
+		}
+		if i < len(combinations)-1 {
+			z01.PrintRune(',')
+			z01.PrintRune(' ')
+		}
+	}
+	z01.PrintRune('\n')
 }
 
 func generateCombinations(n int, start int, prefix string) []string {
-    if n == 0 {
-        return []string{prefix}
-    }
+	if n == 0 {
+		return []string{prefix}
+	}
 
-    combinations := []string{}
-    for i := start; i <= 9; i++ {
-        newPrefix := fmt.Sprintf("%s%d", prefix, i)
-        combinations = append(combinations, generateCombinations(n-1, i+1, newPrefix)...)
-    }
-    return combinations
+	combinations := []string{}
+	for i := start; i <= 9; i++ {
+		newPrefix := fmt.Sprintf("%s%d", prefix, i)
+		combinations = append(combinations, generateCombinations(n-1, i+1, newPrefix)...)
+	}
+	return combinations
 }
