990346d1c985a4b66abe25970ca577f4a03240ad
melfarss <medlfarssi10@gmail.com>
2024-07-24 18:10:57 +0100

    teacher

--- teacher.sh
+++ teacher.sh
@@ -1,17 +1,7 @@
-#!/bin/bash
-
-# Step 1: Find the key interview number
-key_interview=$(grep -oP '(?<=export interview_id=)\d+' interviews/interview-*)
-
-# Step 2: Store the interview number in an environment variable
-export KEY_INTERVIEW=$key_interview
-
-# Step 3: Print the environment variable containing the interview number
-echo "Key Interview Number: $KEY_INTERVIEW"
-
-# Step 4: Print the content of the interview
-echo "Content of the Key Interview:"
-cat interviews/interview-$KEY_INTERVIEW
-
-# Step 5: Print the content of the MAIN_SUSPECT environment variable
-echo "Main Suspect: $MAIN_SUSPECT"
+#! /bin/bash
+interviewn=`grep -H "licen" interviews/* |grep "\"" | cut -f1 -d ":" | rev | cut -f1 -d "-" | rev`
+interview="cat interviews/interview-$interviewn"
+export interviewnum=$interviewn
+echo $interviewnum
+$interview
+echo $MAIN_SUSPECT
