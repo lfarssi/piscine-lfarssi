cdb318acf722cab4e7c3038351050f4c5fe9691a
Mohamed El farssi <medlfarssi10@gmail.com>
2024-07-29 16:27:29 +0100

    isprime

--- isprime.go
+++ isprime.go
@@ -4,13 +4,11 @@ func IsPrime(nb int) bool {
 	var r bool
 	if nb < 0 {
 		r = false
-	}
-	if nb%2 == 0 {
+	} else if nb == 1 {
+		return false
+	} else if nb%2 == 0 {
 		r = false
 	} else if nb%2 != 0 {
-		if nb == 1 {
-			r = false
-		}
 		r = true
 	}
 	return r
