b6bd1cc6a7511adf21161bd8a5bca94027282ab9
Mohamed El farssi <medlfarssi10@gmail.com>
2024-07-29 12:26:24 +0100

    factoriel

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,5 +1,7 @@
 package piscine
 
+const maxInt = 1<<31 - 1
+
 func IterativeFactorial(nb int) int {
 	if nb < 0 {
 		return 0
@@ -11,7 +13,7 @@ func IterativeFactorial(nb int) int {
 
 	fac := 1
 	for i := 1; i <= nb; i++ {
-		if fac > 2147483647/i {
+		if fac > maxInt/i {
 			return 0
 		}
 		fac *= i
