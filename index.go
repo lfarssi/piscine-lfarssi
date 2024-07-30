938f27645413f64ea0c2115b9c1eab55f9cf078d
Mohamed El farssi <medlfarssi10@gmail.com>
2024-07-30 14:39:00 +0100

    index

--- index.go
+++ index.go
@@ -1,15 +1,13 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	var r int
-	si := []rune(s)
-	t := []rune(toFind)
-	for i := range s {
-		if i == t[0] {
-			r = si[i]
-		} else {
-			r = -1
+	if toFind == "" {
+		return 0
+	}
+	for i := 0; i <= len(s)-len(toFind); i++ {
+		if s[i:i+len(toFind)] == toFind {
+			return i
 		}
 	}
-	return r
+	return -1
 }
