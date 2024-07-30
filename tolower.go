abd628d20afb4254048a2bd0bcec48f83a2aacc5
Mohamed El farssi <medlfarssi10@gmail.com>
2024-07-30 16:30:47 +0100

    to upper

---
+++ tolower.go
@@ -0,0 +1,13 @@
+package piscine
+
+func ToUpper(s string) string {
+	var res []rune
+	for _, i := range s {
+		if i >= 'A' && i <= 'Z' {
+			res = append(res, i+32)
+		} else {
+			res = append(res, i)
+		}
+	}
+	return string(res)
+}
