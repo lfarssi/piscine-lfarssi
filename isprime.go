ac4e5917298554e59182d0b3fcd4f4dc2c2757a7
Mohamed El farssi <medlfarssi10@gmail.com>
2024-07-30 11:11:27 +0100

    isprime

--- isprime.go
+++ isprime.go
@@ -2,14 +2,31 @@ package piscine
 
 func IsPrime(nb int) bool {
 	var r bool
-	if nb < 2 {
-		r = false
-	} else if nb == 1 {
+	if nb <= 1 {
 		return false
 	} else if nb == 2 {
 		r = true
-	} else if nb%2 != 0 {
-		r = true
+	} else if nb%2 == 0 {
+		r = false
+	} else {
+		for i := 3; i <= Sqrt(nb); i + 2 {
+			if nb%i == 0 {
+				r = true
+			}
+		}
+	}
+	return r
+}
+
+func Sqrt(nb int) int {
+	if nb == 1 {
+		return 1
+	}
+	r := 0
+	for i := 1; i <= nb; i++ {
+		if i*i == nb {
+			r = i
+		}
 	}
 	return r
 }
