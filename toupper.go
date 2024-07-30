d132b81144a9fe30dfa515741c80835fb26d1b9b
Mohamed El farssi <medlfarssi10@gmail.com>
2024-07-30 16:26:58 +0100

    to upper

--- toupper.go
+++ toupper.go
@@ -3,7 +3,7 @@ package piscine
 func ToUpper(s string) string {
 	var res []rune
 	for _, i := range s {
-		if i < 'a' && i > 'z' {
+		if i >= 'a' && i <= 'z' {
 			res = append(res, i-32)
 		} else {
 			res = append(res, i)
