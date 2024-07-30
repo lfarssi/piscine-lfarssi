ad052441a99139f929e1f2d8bde81fb29ce17296
Mohamed El farssi <medlfarssi10@gmail.com>
2024-07-30 11:58:34 +0100

    next prime

---
+++ findnextprime.go
@@ -0,0 +1,44 @@
+package piscine
+
+func FindNextPrime(nb int) int {
+	if IsPrime(nb) == true {
+		return nb
+	} else {
+		for i := nb; ; i++ {
+			if IsPrime(i) {
+				return i
+			}
+		}
+	}
+}
+
+func IsPrime(nb int) bool {
+	if nb <= 1 {
+		return false
+	}
+	if nb == 2 {
+		return true
+	}
+	if nb%2 == 0 {
+		return false
+	}
+	for i := 3; i <= Sqrtt(nb); i += 2 {
+		if nb%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func Sqrtt(nb int) int {
+	if nb <= 0 {
+		return 0
+	}
+	x := nb
+	y := (x + nb/x) / 2
+	for y < x {
+		x = y
+		y = (x + nb/x) / 2
+	}
+	return x
+}
