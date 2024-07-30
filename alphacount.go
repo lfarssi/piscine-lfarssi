9a5a0e278edff78391afdf7b44a40233ec8e4d31
Mohamed El farssi <medlfarssi10@gmail.com>
2024-07-30 12:55:00 +0100

    alpha count

--- alphacount.go
+++ alphacount.go
@@ -3,9 +3,9 @@ package piscine
 func AlphaCount(s string) int {
 	count := 0
 	for _, i := range s {
-		if i >= 'a' || i <= 'z' || i >= 'A' || i <= 'Z' {
+		if (i >= 'a' || i <= 'z') || (i >= 'A' || i <= 'Z') {
 			count++
-		}
+		}	
 	}
 	return count
 }
