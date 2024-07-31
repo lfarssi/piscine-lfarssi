c03a957996bb4cdb6b4d3948bf6d5b49dd2f6684
MOHAMED EL FARSSI <melfarss@noreply.learn.zone01oujda.ma>
2024-07-31 14:19:15 +0000

    Update 'printprogramname/main.go'

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -4,15 +4,25 @@ import (
 	"os"
 
 	"github.com/01-edu/z01"
-
-	"path/filepath"
 )
 
 func main() {
-	name := os.Args[0]
-	n := filepath.Base(name)
-	for _, val := range n {
-		z01.PrintRune(val)
+	executablePath := os.Args[0]
+
+	var executableName string
+	for i := len(executablePath) - 1; i >= 0; i-- {
+		if executablePath[i] == '/' {
+			executableName = executablePath[i+1:]
+			break
+		}
+	}
+	if executableName == "" {
+		executableName = executablePath
+	}
+
+	runes := []rune(executableName)
+	for i := range runes {
+		z01.PrintRune(runes[i])
 	}
 	z01.PrintRune('\n')
 }
