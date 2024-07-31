d72be458986a483ec633db719be2c9b664d3c55c
MOHAMED EL FARSSI <melfarss@noreply.learn.zone01oujda.ma>
2024-07-31 13:55:50 +0000

    Update 'printprogramname/main.go'

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"github.com/01-edu/z01"
 	"os"
+	"path/filepath"
 )
 
 func main() {
 	name := os.Args[0]
-	for _, val := range name {
+	n := filepath.Base(name)
+
+	for _, val := range n {
 		z01.PrintRune(val)
 	}
 	z01.PrintRune('\n')
