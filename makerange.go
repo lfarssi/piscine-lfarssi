02f0df86252d52d0d3637bc6a63d5b042ed1ff46
Mohamed El farssi <medlfarssi10@gmail.com>
2024-08-01 15:06:38 +0100

    make range

--- makerange.go
+++ makerange.go
@@ -1,12 +1,12 @@
 package piscine
 
 func MakeRange(min, max int) []int {
-	r := make([]int, max-min)
 	if max <= min {
 		return nil
-	}
+		}
+	r := make([]int, max-min)
 	for i := min; i < max; i++ {
-		r[i-min] += i
+		r[i-min] = i
 	}
 	return r
 }
