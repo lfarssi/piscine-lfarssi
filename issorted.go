ee9d147964167f1ab8540617614544ee97b98f6f
Mohamed El farssi <medlfarssi10@gmail.com>
2024-08-06 15:16:27 +0100

    is sorted

--- issorted.go
+++ issorted.go
@@ -1,10 +1,17 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
+	as := true
+	ds := true
 	for i := 0; i < len(a)-1; i++ {
-		if f(a[i], a[i+1]) >= 0 {
-			return false
+		if f(a[i], a[i+1]) < 0 {
+			as = false
 		}
 	}
-	return true
+	for i := 0; i < len(a)-1; i++ {
+		if f(a[i], a[i+1]) > 0 {
+			ds = false
+		}
+	}
+	return as || ds
 }
