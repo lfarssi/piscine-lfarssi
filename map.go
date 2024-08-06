280927959d54f00fe086d5b3a87e19b04c2b74db
Mohamed El farssi <medlfarssi10@gmail.com>
2024-08-06 12:50:19 +0100

    mapping

--- map.go
+++ map.go
@@ -2,8 +2,8 @@ package piscine
 
 func Map(f func(int) bool, a []int) []bool {
 	bl := make([]bool, len(a))
-	for _, i := range a {
-		bl = append(bl, f(i))
+	for i,v := range a {
+		bl[i] = f(v)
 	}
 	return bl
 }
