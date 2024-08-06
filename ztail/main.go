910ab3b12aa3d54f66f8bc8cecf8ae0a9eb54fc7
Mohamed El farssi <medlfarssi10@gmail.com>
2024-08-06 18:01:48 +0100

    ztail

--- ztail/main.go
+++ ztail/main.go
@@ -5,125 +5,74 @@ import (
 	"os"
 )
 
-// parseInt converts a string to an integer manually.
-func parseInt(s string) (int, error) {
-	if len(s) == 0 {
-		return 0, fmt.Errorf("empty string cannot be converted to int")
-	}
-
-	var num int
-	sign := 1
-	start := 0
-
-	if s[0] == '-' {
-		sign = -1
-		start = 1
-	} else if s[0] == '+' {
-		start = 1
-	}
-
-	for i := start; i < len(s); i++ {
-		ch := s[i]
-		if ch < '0' || ch > '9' {
-			return 0, fmt.Errorf("invalid character '%c' in number", ch)
+func PerfAtoi(s string) int {
+	var i int = 0
+	for _, c := range s {
+		if !(c >= '0' && c <= '9') {
+			return 0
 		}
-		num = num*10 + int(ch-'0')
+		i = i*10 + int(c-'0')
 	}
-
-	return num * sign, nil
+	return i
 }
 
-// numberOfBytes processes command-line arguments to extract the number of bytes and file names.
-func numberOfBytes(args []string) (int, []string) {
-	n := len(args)
-	nbytes := 0
-	var files []string
-
-	for i, v := range args {
-		if v == "-c" {
-			if i >= n-1 {
-				fmt.Println("tail: option requires an argument -- 'c'")
-				fmt.Println("Try 'tail --help' for more information.")
-				os.Exit(1)
-			}
-			arg := args[i+1]
-
-			var err error
-			nbytes, err = parseInt(arg)
-			if err != nil {
-				fmt.Printf("tail: invalid number of bytes: %s\n", arg)
-				os.Exit(1)
-			}
-			continue
-		}
-
-		if _, err := parseInt(v); err != nil {
-			files = append(files, v)
+func Atoi(s string) int {
+	var i int = 0
+	if len(s) == 0 || s == "-" || s == "+" {
+		return i
+	}
+	for _, c := range s {
+		if !((c >= '0' && c <= '9') || c == '-' || c == '+') {
+			return i
 		}
 	}
-
-	return nbytes, files
-}
-
-// fileSize retrieves the size of the file.
-func fileSize(fi *os.File) int64 {
-	fileInfo, err := fi.Stat()
-	if err != nil {
-		fmt.Println(err.Error())
-		return 0
+	if s[0] == '-' && s[1] == '-' {
+		return i
 	}
-	return fileInfo.Size()
+	if s[0] == '-' && len(s) != 1 {
+		s = s[1:]
+		i = PerfAtoi(s)
+		i = -i
+	} else if s[0] == '+' && len(s) != 1 {
+		s = s[1:]
+		i = PerfAtoi(s)
+	} else {
+		i = PerfAtoi(s)
+	}
+	return i
 }
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: <program> -c <number_of_bytes> <file1> [<file2> ...]")
-		os.Exit(1)
-	}
-
-	nbytes, files := numberOfBytes(os.Args[1:])
-
-	if nbytes <= 0 {
-		fmt.Println("tail: invalid number of bytes")
-		os.Exit(1)
-	}
-
-	printName := len(files) > 1
-	exitStatus := 0
-
-	for j, fileName := range files {
-		fi, err := os.Open(fileName)
+	args := os.Args[1:]
+	c := args[1]
+	args = args[2:]
+	didWoopsie := false
+	for i := 0; i < len(args); i++ {
+		f, err := os.Open(args[i])
 		if err != nil {
-			fmt.Printf("open %s: %s\n", fileName, err.Error())
-			exitStatus = 1
-			continue
-		}
-		defer fi.Close()
-
-		if printName {
-			if j > 0 {
-				fmt.Println()
+			fmt.Printf(err.Error() + "\n")
+			didWoopsie = true
+		} else {
+			defer f.Close()
+			b, err := os.ReadFile(args[i])
+			if err != nil {
+				fmt.Printf(err.Error() + "\n")
+				didWoopsie = true
+				continue
 			}
-			fmt.Printf("==> %s <==\n", fileName)
-		}
-
-		fileSize := fileSize(fi)
-		if nbytes > int(fileSize) {
-			nbytes = int(fileSize)
-		}
-
-		read := make([]byte, nbytes)
-		_, err = fi.ReadAt(read, fileSize-int64(nbytes))
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-
-		fmt.Print(string(read))
-
-		if j < len(files)-1 {
-			fmt.Println()
+			if Atoi(c) < len(b) {
+				b = b[len(b)-Atoi(c):]
+			}
+			if i != 0 {
+				fmt.Printf("\n")
+			}
+			fmt.Printf("==> " + args[i] + " <==\n")
+			fmt.Printf(string(b))
 		}
 	}
-
-	os.Exit(exitStatus)
+	if didWoopsie {
+		os.Exit(1)
+	} else {
+		os.Exit(0)
+	}
 }
