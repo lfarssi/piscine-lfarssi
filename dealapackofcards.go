bdabed4e17740389a47e751eb78ce29c0dcec8c1
Mohamed El farssi <medlfarssi10@gmail.com>
2024-08-07 17:30:34 +0100

    cards

---
+++ dealapackofcards.go
@@ -0,0 +1,21 @@
+package piscine
+
+import "fmt"
+
+func DealAPackOfCards(deck []int) {
+	var player int = 4
+	var Card int = 3
+	for i := 0; i < player ; i++ {
+		start := i * Card
+		end := start + Card
+		card := deck[start:end]
+		fmt.Printf("player : %d", i+1)
+		for j, v := range card {
+			if j > 0 {
+				print(", ")
+			}
+			print(v)
+		}
+		fmt.Println()
+	}
+}
