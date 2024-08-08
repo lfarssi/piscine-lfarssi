88a06b3f1b004dc57eb5d8ab057bf73324f871a6
Mohamed El farssi <medlfarssi10@gmail.com>
2024-08-08 07:36:48 +0100

    rock and roll

--- rockandroll.go
+++ rockandroll.go
@@ -2,7 +2,7 @@ package piscine
 
 func RockAndRoll(n int) string {
 	if n < 0 {
-		return "error: non divisible\n"
+		return "error: number is negative\n"
 	} else if n%2 == 0 && n%3 == 0 {
 		return "rock and roll\n"
 	} else if n%3 == 0 {
