5a349c3ff42612676fdb69fef41b076a9fe62402
Mohamed El farssi <medlfarssi10@gmail.com>
2024-08-08 08:19:47 +0100

    bread

--- loafofbread.go
+++ loafofbread.go
@@ -4,23 +4,25 @@ func LoafOfBread(str string) string {
 	if len(str) < 5 {
 		return "Invalid Output\n"
 	}
-	var s string
-	var r []rune
-	for _, i := range str {
-		if i != 32 {
-			r = append(r, i)
+	var result string
+	var word []rune
+	i := 0
+	for i < len(str) {
+		if str[i] != ' ' {
+			word = append(word, rune(str[i]))
 		}
-		if len(r) == 5 {
-			s += string(r) + " "
-			r = nil
-			if len(str) > 0 {
-				str = str[1:]
-			}
+
+		if len(word) == 5 {
+			result += string(word) + " "
+			word = nil
+			i++
 		}
+
+		i++
 	}
-	if len(s) > 0 && s[len(s)-1] == ' ' {
-		s = s[:len(s)-1]
+	if len(result) > 0 && result[len(result)-1] == ' ' {
+		result = result[:len(result)-1]
 	}
-	s += "\n"
-	return s
+	result += "\n"
+	return result
 }
