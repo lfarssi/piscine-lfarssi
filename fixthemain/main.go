747773a38ed6e34afc3acf06358555beda80238d
Mohamed El farssi <medlfarssi10@gmail.com>
2024-08-08 05:18:06 +0100

    open the door

---
+++ fixthemain/main.go
@@ -0,0 +1,56 @@
+package main
+
+import "github.com/01-edu/z01" // Import z01 package for PrintRune
+
+// Define Door type and states
+type Door struct {
+	state string
+}
+
+const (
+	OPEN  = "open"
+	CLOSE = "close"
+)
+
+func PrintStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+}
+
+func CloseDoor(ptrDoor *Door) bool {
+	PrintStr("Door Closing...")
+	ptrDoor.state = CLOSE
+	return true
+}
+
+func IsDoorOpen(ptrDoor *Door) bool {
+	PrintStr("Is the Door opened? ")
+	return ptrDoor.state == OPEN
+}
+
+func IsDoorClose(ptrDoor *Door) bool {
+	PrintStr("Is the Door closed? ")
+	return ptrDoor.state == CLOSE
+}
+
+func OpenDoor(ptrDoor *Door) bool {
+	PrintStr("Door Opening...")
+	ptrDoor.state = OPEN
+	return true
+}
+
+func main() {
+	door := &Door{}
+
+	OpenDoor(door)
+	if IsDoorClose(door) {
+		OpenDoor(door)
+	}
+	if IsDoorOpen(door) {
+		CloseDoor(door)
+	}
+	if door.state == OPEN {
+		CloseDoor(door)
+	}
+}
