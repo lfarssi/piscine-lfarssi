bbc3465531588f344388cfd293862a1cdfc285bd
Mohamed El farssi <medlfarssi10@gmail.com>
2024-08-08 08:20:57 +0100

    br

--- loafofbread.go
+++ loafofbread.go
@@ -4,25 +4,33 @@ func LoafOfBread(str string) string {
 	if len(str) < 5 {
 		return "Invalid Output\n"
 	}
+
 	var result string
 	var word []rune
 	i := 0
-	for i < len(str) {
-		if str[i] != ' ' {
+	n := len(str)
+
+	for i < n {
+		if str[i] != ' ' { // Skip spaces
 			word = append(word, rune(str[i]))
 		}
 
-		if len(word) == 5 {
+		if len(word) == 5 { // When we have 5 characters
 			result += string(word) + " "
-			word = nil
-			i++
+			word = nil // Reset word for the next segment
+			i++        // Move past the character we just processed
 		}
 
 		i++
 	}
+
+	// Remove trailing space if it exists
 	if len(result) > 0 && result[len(result)-1] == ' ' {
 		result = result[:len(result)-1]
 	}
+
+	// Add newline character at the end of the result
 	result += "\n"
+
 	return result
 }
