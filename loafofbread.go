2a5de2d8b0f659438a7799132422852c0251ebcf
Mohamed El farssi <medlfarssi10@gmail.com>
2024-08-08 08:23:17 +0100

    br

--- loafofbread.go
+++ loafofbread.go
@@ -1,36 +1,31 @@
 package piscine
 
 func LoafOfBread(str string) string {
-	if len(str) < 5 {
+	if len(str) == 0 {
+		return "\n"
+	} else if len(str) < 5 {
 		return "Invalid Output\n"
-	}
-
-	var result string
-	var word []rune
-	i := 0
-	n := len(str)
-
-	for i < n {
-		if str[i] != ' ' { // Skip spaces
-			word = append(word, rune(str[i]))
+	} else {
+		skip := ""
+		i := 0
+		for _, e := range str {
+			if i%6 != 5 && e == ' ' {
+				continue
+			}
+			if i%6 == 5 {
+				skip += " "
+			} else {
+				skip += string(e)
+			}
+			i++
 		}
-
-		if len(word) == 5 { // When we have 5 characters
-			result += string(word) + " "
-			word = nil // Reset word for the next segment
-			i++        // Move past the character we just processed
+		// remove trailing spaces
+		for i := len(skip) - 1; i >= 0; i-- {
+			if skip[i] != ' ' {
+				skip = skip[:i+1]
+				break
+			}
 		}
-
-		i++
+		return skip + "\n"
 	}
-
-	// Remove trailing space if it exists
-	if len(result) > 0 && result[len(result)-1] == ' ' {
-		result = result[:len(result)-1]
-	}
-
-	// Add newline character at the end of the result
-	result += "\n"
-
-	return result
 }
