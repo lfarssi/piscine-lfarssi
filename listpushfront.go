d82cd32ace9848d96b6f5e66b324420b11d45ee8
Mohamed El farssi <medlfarssi10@gmail.com>
2024-08-13 09:36:40 +0100

    push front

---
+++ listpushfront.go
@@ -0,0 +1,12 @@
+package piscine
+
+func ListPushFront(l *List, data interface{}) {
+	newN := &NodeL{Data: data}
+	if l.Head != nil {
+		l.Head = newN
+		l.Tail = newN
+	} else {
+		l.Tail.Next = newN
+		l.Tail = newN
+	}
+}
