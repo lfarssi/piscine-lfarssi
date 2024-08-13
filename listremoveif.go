f56d4a1421e96a6b4d37898eed5ab1cfee78108a
Christopher  Fremond <dev@01-edu.org>
2024-08-13 11:58:35 +0000

    Update 'listremoveif.go'

--- listremoveif.go
+++ listremoveif.go
@@ -1,20 +1,51 @@
 package piscine
+
 type NodeL struct {
-	Data interface{}
-	Next *NodeL
+    Data interface{}
+    Next *NodeL
 }
 
 type List struct {
-	Head *NodeL
-	Tail *NodeL
+    Head *NodeL
+    Tail *NodeL
 }
-func ListRemoveIf(l *List, data_ref interface{}) {
-  current := l.Head
-  for current.Next != nil {
-    if current.Data == data_ref {
-      current.Data = nil
+
+func ListPushBack(l *List, data interface{}) {
+    newNode := &NodeL{Data: data}
+    if l.Head == nil {
+        l.Head = newNode
+        l.Tail = newNode
+    } else {
+        l.Tail.Next = newNode
+        l.Tail = newNode
     }
-    current = current.Next
-  }
-  return current.Data
+}
+
+func ListRemoveIf(l *List, data_ref interface{}) {
+    if l.Head == nil {
+        return
+    }
+
+    // Remove nodes from the head
+    for l.Head != nil && l.Head.Data == data_ref {
+        l.Head = l.Head.Next
+    }
+    
+    if l.Head == nil {
+        l.Tail = nil
+        return
+    }
+
+    // Remove nodes in the middle
+    current := l.Head
+    for current != nil && current.Next != nil {
+        if current.Next.Data == data_ref {
+            current.Next = current.Next.Next
+            if current.Next == nil {
+                l.Tail = current
+            }
+        } else {
+            current = current.Next
+        }
+    }
 }
