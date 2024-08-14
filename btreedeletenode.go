071b3a886c3b5b0cf02683c4bd6e0fe20bf13793
Christopher  Fremond <dev@01-edu.org>
2024-08-14 15:30:45 +0000

    Update 'btreedeletenode.go'

--- btreedeletenode.go
+++ btreedeletenode.go
@@ -1,5 +1,10 @@
 package piscine
 
+type TreeNode struct {
+	Left, Right, Parent *TreeNode
+	Data                 string
+}
+
 func BTreeDeleteNode(root, nodeToRemove *TreeNode) *TreeNode {
 	if nodeToRemove == nil {
 		return root
@@ -24,4 +29,14 @@ func BTreeDeleteNode(root, nodeToRemove *TreeNode) *TreeNode {
 		}
 	}
 	return root
+}
+
+func BTreeMin(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Left == nil {
+		return root
+	}
+	return BTreeMin(root.Left)
 }
