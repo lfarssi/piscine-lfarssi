0f3d054b663ece82bac7103b273336da535d1da0
Mohamed El farssi <medlfarssi10@gmail.com>
2024-08-14 15:33:56 +0100

    tree insert

--- btreeinsertdata.go
+++ btreeinsertdata.go
@@ -8,6 +8,7 @@ type TreeNode struct {
 // BTreeInsertData inserts a new node with the given data into the binary search tree
 func BTreeInsertData(root *TreeNode, data string) *TreeNode {
 	if root == nil {
+		// If the tree is empty, create a new node and return it as the root
 		return &TreeNode{Data: data}
 	}
 
@@ -16,17 +17,20 @@ func BTreeInsertData(root *TreeNode, data string) *TreeNode {
 	insert = func(node *TreeNode, data string) *TreeNode {
 		if data < node.Data {
 			if node.Left == nil {
+				// Insert the new node as the left child
 				node.Left = &TreeNode{Data: data, Parent: node}
 				return node.Left
 			}
 			return insert(node.Left, data)
 		} else if data > node.Data {
 			if node.Right == nil {
+				// Insert the new node as the right child
 				node.Right = &TreeNode{Data: data, Parent: node}
 				return node.Right
 			}
 			return insert(node.Right, data)
 		}
+		// If data == node.Data, we do nothing as it's a duplicate
 		return nil
 	}
 
