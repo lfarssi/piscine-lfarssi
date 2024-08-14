337649263428d91810a617a120c381b80a972a39
Mohamed El farssi <medlfarssi10@gmail.com>
2024-08-14 15:26:28 +0100

    merge list

--- listremoveif.go
+++ listremoveif.go
@@ -1,51 +1,40 @@
 package piscine
 
 type NodeL struct {
-    Data interface{}
-    Next *NodeL
+	Data interface{}
+	Next *NodeL
 }
 
 type List struct {
-    Head *NodeL
-    Tail *NodeL
-}
-
-func ListPushBack(l *List, data interface{}) {
-    newNode := &NodeL{Data: data}
-    if l.Head == nil {
-        l.Head = newNode
-        l.Tail = newNode
-    } else {
-        l.Tail.Next = newNode
-        l.Tail = newNode
-    }
+	Head *NodeL
+	Tail *NodeL
 }
 
 func ListRemoveIf(l *List, data_ref interface{}) {
-    if l.Head == nil {
-        return
-    }
+	if l.Head == nil {
+		return
+	}
 
-    // Remove nodes from the head
-    for l.Head != nil && l.Head.Data == data_ref {
-        l.Head = l.Head.Next
-    }
-    
-    if l.Head == nil {
-        l.Tail = nil
-        return
-    }
+	// Remove nodes from the head
+	for l.Head != nil && l.Head.Data == data_ref {
+		l.Head = l.Head.Next
+	}
 
-    // Remove nodes in the middle
-    current := l.Head
-    for current != nil && current.Next != nil {
-        if current.Next.Data == data_ref {
-            current.Next = current.Next.Next
-            if current.Next == nil {
-                l.Tail = current
-            }
-        } else {
-            current = current.Next
-        }
-    }
+	if l.Head == nil {
+		l.Tail = nil
+		return
+	}
+
+	// Remove nodes in the middle
+	current := l.Head
+	for current != nil && current.Next != nil {
+		if current.Next.Data == data_ref {
+			current.Next = current.Next.Next
+			if current.Next == nil {
+				l.Tail = current
+			}
+		} else {
+			current = current.Next
+		}
+	}
 }
